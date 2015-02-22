6d3292ab2e3bfe4667939dbcf777080725160b2e
Kevin Streit <streit@cs.uni-saarland.de>
2015-02-23 00:02:07 +0100

    Add argument to gen_symlinks script

--- gen_symlinks.sh
+++ gen_symlinks.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 
-ln -s `pwd`/fasdrc ~/.fasdrc
-ln -s `pwd`/gitconfig ~/.gitconfig
-ln -s `pwd`/gitignore ~/.gitignore
-ln -s `pwd`/tmux.conf ~/.tmux.conf
-ln -s `pwd`/tmux-powerline ~/.tmux-powerline
-ln -s `pwd`/vim ~/.vim
-ln -s `pwd`/vimrc ~/.vimrc
-ln -s `pwd`/zshenv ~/.zshenv
-ln -s `pwd`/zshrc ~/.zshrc
-ln -s `pwd`/zpreztorc ~/.zpreztorc
+TGT_DIR = $1
+
+ln -s `pwd`/fasdrc $TGT_DIR/.fasdrc
+ln -s `pwd`/gitconfig $TGT_DIR/.gitconfig
+ln -s `pwd`/gitignore $TGT_DIR/.gitignore
+ln -s `pwd`/tmux.conf $TGT_DIR/.tmux.conf
+ln -s `pwd`/tmux-powerline $TGT_DIR/.tmux-powerline
+ln -s `pwd`/vim $TGT_DIR/.vim
+ln -s `pwd`/vimrc $TGT_DIR/.vimrc
+ln -s `pwd`/zshenv $TGT_DIR/.zshenv
+ln -s `pwd`/zshrc $TGT_DIR/.zshrc
+ln -s `pwd`/zpreztorc $TGT_DIR/.zpreztorc
 
